09d2d157a5ea70aac3273dd495c0632be6fa77b0
Martin Hentschel <hemartin@Martins-MBP.lan>
2017-04-03 23:00:59 -0700

    inital upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # hn-responder
 Script that replies to every @newsycombinator tweet with a link to the comments page
+
+## Setup
+
+Install Python modules `requests-oauthlib` and `python-firebase`:
+```
+pip install requests-oauthlib
+pip install python-firebase
+```
+
+Create a Twitter app on https://apps.twitter.com/app/
+
+Get tokens and secrets for your app on "Keys and Access Tokens" page.
+
+Create file `twitter-secrets.json` with following contents (replace with your actual tokens and secrets):
+```
+{
+    "access_token": "12345",
+    "access_token_secret": "abcdefg",
+    "consumer_key": "1q2w3e4",
+    "consumer_secret": "ABCDEFG"
+}
+```
+
+Execute script from command line. Note that it will run forever until killed.
+```
+python hn-responder.py
+```
---
+++ hn-responder.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+import json
+import re
+import requests
+from Queue import Queue
+from requests_oauthlib import OAuth1
+from threading import Thread
+from firebase import firebase
+
+with open('twitter-secrets.json') as twitter_secrets:
+    s = json.load(twitter_secrets)
+    access_token = s['access_token']
+    access_token_secret = s['access_token_secret']
+    consumer_key = s['consumer_key']
+    consumer_secret = s['consumer_secret']
+auth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret)
+
+firebase = firebase.FirebaseApplication(
+    'https://hacker-news.firebaseio.com', None)
+
+work_queue = Queue()
+
+
+def read_tweets():
+    url = 'https://userstream.twitter.com/1.1/user.json'
+    r = requests.get(url, auth=auth, stream=True)
+    for line in r.iter_lines():
+        if line:
+            tweet = json.loads(line.decode('utf-8'))
+            work_queue.put(tweet)
+
+
+def process(tweet):
+    if 'user' in tweet and tweet['user']['id'] == 14335498:
+        tweet_id = tweet['id']
+        title = re.sub('https://t.co.*$', '', tweet['text']).strip()
+        hackernews_id = find_hackernews_id(title)
+        hackernews_url = 'https://news.ycombinator.com/item?id=' + str(item_id)
+        post_tweet(tweet_id, title)
+
+
+def find_hackernews_id(title):
+    top_ids = firebase.get('/v0/topstories', None)
+    for item_id in top_ids:
+        item = firebase.get('/v0/item', item_id)
+        print item['title']
+        if 'title' in item and item['title'].strip() == title:
+            return item_id
+
+
+def post_tweet(tweet_id, hackernews_url):
+    url = 'https://api.twitter.com/1.1/statuses/update.json'
+    tweet_text = '@newsycombinator comments ' + hackernews_url
+    params = {'status': tweet_text, 'in_reply_to_status_id': tweet_id}
+    requests.post(url, auth=auth, data=params)
+
+
+def run():
+    tweet = work_queue.get()
+    process(tweet)
+    work_queue.task_done()
+
+
+t = Thread(target=run)
+t.daemon = True
+t.start()
+
+read_tweets()
+work_queue.join()
